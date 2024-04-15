703822f7c00367fab00653703836e3f0b5ba90d0
Frankystan <fffernandez84@outlook.com>
2024-04-16 01:50:29 +0200

    #4

--- src/app/layout/fab-scroll-to-top/fab-scroll-to-top.component.ts
+++ src/app/layout/fab-scroll-to-top/fab-scroll-to-top.component.ts
@@ -1,67 +1,29 @@
 import { ScrollDispatcher, ScrollingModule } from '@angular/cdk/scrolling';
-import { DOCUMENT, ViewportScroller } from '@angular/common';
-import { ThisReceiver } from '@angular/compiler';
-import {
-    Component,
-    Input,
-    Signal,
-    effect,
-    inject,
-    signal,
-} from '@angular/core';
+import { Component, Signal, effect, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { Observable, fromEvent, of } from 'rxjs';
-import { distinctUntilChanged, map, tap } from 'rxjs/operators';
+import { map, distinctUntilChanged } from 'rxjs';
 
 @Component({
-    selector: 'app-fab-scroll-to-top',
-    standalone: true,
-    imports: [MatIconModule, MatButtonModule, ScrollingModule],
-    template: `
-        @if (isVisibe) {
-            <button
-                mat-mini-fab
-                class="bottom-right"
-                color="primary"
-                aria-label="Scroll to top button"
-                (click)="scrollToTop()"
-            >
-                <mat-icon>arrow_upward</mat-icon>
-            </button>
-        }
-    `,
-    styles: [
-        `
-            .bottom-right {
-                bottom: 5.5rem;
-                cursor: pointer;
-                float: right;
-                left: auto;
-                opacity: 0.7;
-                position: fixed;
-                right: 1.5rem;
-                z-index: 1;
-            }
-        `,
-    ],
+  selector: 'app-fab-scroll-to-top',
+  standalone: true,
+  imports: [MatIconModule, MatButtonModule, ScrollingModule],
+  templateUrl: './fab-scroll-to-top.component.html',
+  styleUrl: './fab-scroll-to-top.component.scss'
 })
 export class FabScrollToTopComponent {
+
     #scrollDispatcher = inject(ScrollDispatcher);
-
-    private readonly vScroller = inject(ViewportScroller);
-
-    show = signal(false);
 
     offSet: number = 300;
     lastPosition: number = 0;
     currentPosition: number = 0;
-    isVisibe: boolean = false;
+    isVisible: boolean = false;
 
     constructor() {
         effect(() => {
-            this.isVisibe = this.visible();
+            this.isVisible = this.visible();
         });
     }
 
@@ -81,17 +43,17 @@ export class FabScrollToTopComponent {
 
                 switch (true) {
                     case this.currentPosition == 0: // TOP
-                        show= false;
+                        show = false;
                         break;
                     case this.currentPosition > this.lastPosition: // DOWN
-                        show= false;
+                        show = false;
                         break;
                     case this.lastPosition - this.offSet >= 0: // UP
-                       show= true;
+                        show = true;
                         break;
 
                     default:
-                        show= false;
+                        show = false;
                         break;
                 }
 
