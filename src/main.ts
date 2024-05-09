f3c1c2b40723e376c370e9b0433735eef9b07700
Frankystan <fffernandez84@outlook.com>
2024-05-09 21:00:54 +0200

    #54 load Flag Icons frm local to eagerly load svg

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 import 'zone.js';
 
+/*
+LOAD FLAG ICONS from node_modules, FIX 
+usar ambas soluciones:
+https://github.com/lipis/flag-icons/issues/1142#issuecomment-1806073320
+https://github.com/lipis/flag-icons/issues/1142#issuecomment-1806814863
+*/
+
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
     console.error(err),
 );
